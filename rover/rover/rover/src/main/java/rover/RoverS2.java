43be33e4db983d7e6f7ede955cf92d6d38f62ef5
thomas ae smith <thomas.ae.smith@googlemail.com>
2013-12-10 03:55:47 +0000

    Agent for Scenario 2 complete.

--- rover/rover/rover/src/main/java/rover/RoverS2.java
+++ rover/rover/rover/src/main/java/rover/RoverS2.java
@@ -1,7 +1,6 @@
 package rover;
 
-import java.util.Random;
-
+import java.util.Stack;
 import org.iids.aos.log.Log;
 
 
@@ -25,8 +24,8 @@ public class RoverS2 extends Rover {
 	private double homeX;
 	private double homeY;
 	// relative offset from home to target
-	private double targetX;
-	private double targetY;
+	private Stack<Double> targetX;
+	private Stack<Double> targetY;
 	// current FSM state
 	private int state;
 
@@ -72,6 +71,9 @@ public class RoverS2 extends Rover {
 			PATHY[i] = -Math.cos(i*Math.PI/3.0) * 16.0/5.0 * SCAN;
 			// Log.console("" + i + ": " + PATHX[i] + ", " + PATHY[i]);
 		}
+
+		targetX = new Stack<Double>();
+		targetY = new Stack<Double>();
 	}
 
 	@Override
@@ -144,30 +146,25 @@ public class RoverS2 extends Rover {
 		case PollResult.SCAN:
 			Log.console(STATES[state] + " scan complete");
 
-			//temp variable for if we find something
-			ScanItem si = null;
 			for(ScanItem item : pr.getScanItems()) {
 				if(item.getItemType() == ScanItem.RESOURCE) {
 					Log.console("Resource found at: " + item.getxOffset() + ", " + item.getyOffset());
-					//store reference to the found resource
-					si = item;
 					//store offset relative to home
-					// targetX = item.getxOffset() - homeX;
-					// targetY = item.getyOffset() - homeY;
-					//switch into rescue mode and cease scanning (in scenario #1)
+					targetX.push(item.getxOffset() - homeX);
+					targetY.push(item.getyOffset() - homeY);
+					//switch into rescue mode and continue
 					state = RESCUE;
-					break;
 				}
 			}
 			//if we didn't find anything
-			if (si == null) {
+			if (targetX.empty()) {
 				getNextPatrolPoint();
 			}
 
 			//go to the found item if extant, or the target otherwise
 			try {
 				Log.console("Moving...");
-				gotoXY((si != null)? si.getxOffset() : homeX+targetX, (si != null)? si.getyOffset() : homeY+targetY);
+				gotoTarget();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -190,7 +187,9 @@ public class RoverS2 extends Rover {
 			if (state == RETURN) {
 				try {
 					Log.console("Returning home...");
-					gotoXY(homeX, homeY);
+					targetX.push(0.0);
+					targetY.push(0.0);
+					gotoTarget();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -203,30 +202,34 @@ public class RoverS2 extends Rover {
 				try {
 					Log.console("Depositing more...");
 					deposit();
+					return;
 				} catch (Exception e) {
 					e.printStackTrace();
-					state = PATROL;
 				}
-			} else {
-				state = PATROL;
 			}
-			
-			if (state == PATROL) {
-				try {
-					Log.console("Patrolling more...");
-					//(we should already be at home, here, but just in case add the home offset...)
-					gotoXY(homeX+targetX, homeY+targetY);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			if (targetX.empty()) {
+				getNextPatrolPoint();
+				state = PATROL;
+			} else {
+				state = RESCUE;
+			}
+		
+			try {
+				Log.console(STATES[state] + " some more...");
+				//(we should already be at home, here, but just in case add the home offset...)
+				gotoTarget();
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 			break;
 		}
 		
 	}
 
-	void gotoXY(double xOffset, double yOffset) throws Exception {
+	void gotoTarget() throws Exception {
 		//C = B-A, or something like that. Vectors.
+		double xOffset = targetX.pop() + homeX;
+		double yOffset = targetY.pop() + homeY;
 		homeX -= xOffset;
 		homeY -= yOffset;
 		move(xOffset, yOffset, SPEED);
@@ -243,8 +246,13 @@ public class RoverS2 extends Rover {
 				rank += 1;
 			}
 		}
-		targetX = rank * PATHX[hour] + minute * PATHX[(hour+2)%HOURMAX];
-		targetY = rank * PATHY[hour] + minute * PATHY[(hour+2)%HOURMAX];
+		targetX.push(rank * PATHX[hour] + minute * PATHX[(hour+2)%HOURMAX]);
+		targetY.push(rank * PATHY[hour] + minute * PATHY[(hour+2)%HOURMAX]);
+
+		if (Math.abs(targetX.peek()) > 0.6 * getWorldWidth() || Math.abs(targetY.peek()) > 0.6 * getWorldHeight()) {
+			Log.console("Skipping overlapped patrol node.");
+			getNextPatrolPoint();
+		}
 		// Log.console("It's day " + rank + " at " + hour + ":" + minute + "; going to " + targetX + ", " + targetY);
 
 	}
