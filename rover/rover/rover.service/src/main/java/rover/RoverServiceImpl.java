cb6a9eb227ebcd9c2891de277dce4a96b94554f4
thomas ae smith <thomas.ae.smith@googlemail.com>
2013-12-10 06:19:37 +0000

    Agent for Scenario 4 complete.

--- rover/rover/rover.service/src/main/java/rover/RoverServiceImpl.java
+++ rover/rover/rover.service/src/main/java/rover/RoverServiceImpl.java
@@ -56,7 +56,7 @@ public class RoverServiceImpl extends AbstractDefaultService implements
 		
 		//monitors = new ArrayList<RoverMonitor>();
 		
-		resetWorld(2);
+		resetWorld(3);
 	}
 	
 	@Override
